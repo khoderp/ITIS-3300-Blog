2fb0ea79e650caa45bbd604993dee849c24f9035
jadenPete <jadenpete@github.com>
2023-09-10 19:40:29 -0400

    Moved the build stage of run_production.sh into build_production.sh

---
+++ build_production.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mkdir -p static/scss
+
+node_modules/.bin/webpack build --mode production
+node_modules/.bin/sass static/scss:static/css
--- run_production.sh
+++ run_production.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-mkdir -p static/scss
-
-node_modules/.bin/webpack build --mode production
-node_modules/.bin/sass static/scss:static/css
-
 gunicorn app:app
